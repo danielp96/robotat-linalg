ea510095402e4bdf5924183dc39224b61070830b
Daniel Pineda <daniel-p96@outlook.com>
2022-08-09 10:42:37 -0600

    Check for symmetric and upper/lower hessenberg matrices.

--- include/matf32/matf32_check.h
+++ include/matf32/matf32_check.h
@@ -14,6 +14,7 @@
   * Dependencies.
   */
 #include "matf32_def.h"
+#include "matf32/math_util.h"
 
 // ====================================================================================================
 // Matrix datatype-based checks
@@ -29,7 +30,8 @@
  *              true  :     Operation successful.
  *              false :     Matrix is not square.
  */
-inline bool
+// static keyword fixes explained error of undefined ref when incuding constants.h in math_util.h
+static inline bool 
 matf32_check_square_matrix(const matf32_t* const p_mat)
 {
     return (p_mat->num_cols == p_mat->num_rows);
@@ -73,5 +75,41 @@ matf32_check_triangular_lower(const matf32_t* const p_mat);
 bool
 matf32_is_equal(const matf32_t* const p_mat_a, const matf32_t* const p_mat_b);
 
+
+/**
+ * @brief Checks if a matrix is symmetric
+ *
+ * @param[in]   p_mat Points to matrix to check.
+ *
+ * @return  Execution status
+ *              true  :     Matrix is symmetrical.
+ *              false :     Matrix is not symmetrical.
+ */
+bool
+matf32_check_symmetric(const matf32_t* const p_mat);
+
+/**
+ * @brief Checks if a matrix is upper hessenberg.
+ *
+ * @param[in]   p_mat Points to matrix to check.
+ *
+ * @return  Execution status
+ *              true  :     Matrices is upper hessenberg.
+ *              false :     Matrices is not upper hessenberg.
+ */
+bool
+matf32_check_hessenberg_upper(const matf32_t* const p_mat);
+
+/**
+ * @brief Checks if a matrix is lower hessenberg.
+ *
+ * @param[in]   p_mat Points to matrix to check.
+ *
+ * @return  Execution status
+ *              true  :     Matrices is lower hessenberg.
+ *              false :     Matrices is not lower hessenberg.
+ */
+bool
+matf32_check_hessenberg_lower(const matf32_t* const p_mat);
 
 #endif // ROBOTAT_MATF32_CHECK_H_
--- src/matf32/matf32_check.c
+++ src/matf32/matf32_check.c
@@ -5,9 +5,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#include "matf32/matf32_def.h"
 #include "matf32/matf32_check.h"
-#include "matf32/math_util.h"
 
 
 // ====================================================================================================
@@ -77,4 +75,85 @@ matf32_is_equal(const matf32_t* const p_mat_a, const matf32_t* const p_mat_b)
     }
 #endif
     return is_equal(p_mat_a->p_data, p_mat_b->p_data, p_mat_a->num_rows * p_mat_a->num_cols);
+}
+
+
+bool
+matf32_check_symmetric(const matf32_t* const p_mat)
+{
+#ifdef MATH_MATRIX_CHECK
+    if (matf32_check_square_matrix(p_mat))
+    {
+        return false;
+    }
+#endif
+
+    float* p_data_source = p_mat->p_data;
+
+    uint16_t size = p_mat->num_cols*p_mat->num_rows -1;
+
+    for (uint16_t i = 0; i < size; ++i)
+    {
+        // change != with a comparation with error (abs(a-b) < err)
+        if (p_data_source[i] != p_data_source[size-i])
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
+bool
+matf32_check_hessenberg_upper(const matf32_t* const p_mat)
+{
+#ifdef MATH_MATRIX_CHECK
+    if (p_mat->num_rows != p_mat->num_cols)
+    {
+        return false;
+    }
+#endif
+
+    float* p_data_src = p_mat->p_data;
+
+    for (uint16_t i = 2; i < p_mat->num_rows; ++i)
+    {
+        for (uint16_t j = 0; j < i-1; ++j)
+        {
+            if (0 != p_data_src[i*p_mat->num_rows + j])
+            {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+
+bool
+matf32_check_hessenberg_lower(const matf32_t* const p_mat)
+{
+#ifdef MATH_MATRIX_CHECK
+    if (p_mat->num_rows != p_mat->num_cols)
+    {
+        return false;
+    }
+#endif
+
+    float* p_data_src = p_mat->p_data;
+
+    for (uint16_t i = 0; i < p_mat->num_rows-2; ++i)
+    {
+        for (uint16_t j = p_mat->num_cols-1; j > i+1; --j)
+        {
+            if (0 != p_data_src[i*p_mat->num_rows + j])
+            {
+                //return false;
+            }
+        }
+    }
+
+    return true;
 }
