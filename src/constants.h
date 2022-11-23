47295ca73447445bf81079a220422cb1f75a43ee
Daniel Pineda <daniel-p96@outlook.com>
2022-11-23 11:56:31 -0600

    Reduce default matrix and vector size to fit devices memory.

--- src/constants.h
+++ src/constants.h
@@ -12,7 +12,7 @@
 // Constant macro definitions
 // ====================================================================================================
 #define MAX_ITERATION_COUNT_SVD (30)    /**< Maximum number of iterations for svd_jacobi_one_sided.c */
-#define MAX_VEC_SIZE            (100)   /**< Maximum number of elements allowed for a single row vector. */
+#define MAX_VEC_SIZE            (10)   /**< Maximum number of elements allowed for a single row vector. */
 #define MAX_MAT_SIZE            (MAX_VEC_SIZE*MAX_VEC_SIZE)     /**< Maximum number of elements allowed for a matrix. */
 #define MATH_MATRIX_CHECK               /**< Comment this to disable matrix size checking. */
 #define MATH_EQUAL_PRECISION    (1E-6)  /**< Precision of equal comparisons. */
